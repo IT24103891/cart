7928c5e8cb4d12a7ae7f9f4c6cfe8280d58af863
IT24103891 <it24103891@my.sliit.lk>
2025-05-24 16:19:16 +0530

    it24103891 - cart
    it24103891 - cart

--- src/main/java/com/example/demo/DemoAppApplication.java
+++ src/main/java/com/example/demo/DemoAppApplication.java
@@ -1,13 +1,13 @@
-package com.example.shoppingcartapp;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoppingCartAppApplication {
+public class DemoAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShoppingCartAppApplication.class, args);
+        SpringApplication.run(DemoAppApplication.class, args);
     }
 
 }
